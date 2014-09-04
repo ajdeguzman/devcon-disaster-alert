53aad499d564bd6c073f9715a7e7125e042104c7
ajdeguzman <aljohndeguzman@gmail.com>
2014-09-04 12:41:03 +0800

    binded spinner with resources

--- DevconDevise/src/com/devcon/devise/MenuReport.java
+++ DevconDevise/src/com/devcon/devise/MenuReport.java
@@ -11,16 +11,11 @@ import android.widget.Spinner;
 public class MenuReport extends Fragment {
 	
 	public MenuReport(){}
-	private Spinner spinnerDisasterType;
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
  
         View root = inflater.inflate(R.layout.layout_report, container, false);
-        spinnerDisasterType = (Spinner) root.findViewById(R.id.spinnerTypeDisaster);
-        ArrayAdapter<CharSequence> adapter =ArrayAdapter.createFromResource(getActivity(), R.array.arr_disaster_items, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerDisasterType.setAdapter(adapter);
         return root;
     }
 }
