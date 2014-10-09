02c4699a89b60e058f7c0a5c687905ab00be9d2b
ajdeguzman <aljohndeguzman@gmail.com>
2014-10-09 14:58:20 +0800

    added geocoding

--- DevconDevise/src/com/devcon/devise/SubmitPhoto.java
+++ DevconDevise/src/com/devcon/devise/SubmitPhoto.java
@@ -1,16 +1,23 @@
 package com.devcon.devise;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
+import android.location.Address;
 import android.location.Geocoder;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.parse.Parse;
 import com.parse.ParseFile;
@@ -26,7 +33,7 @@ public class SubmitPhoto extends Activity {
 	private ProgressDialog pd;
 	GPSTracker gps;
 	double latitude,longitude;
-	Geocoder geocoder;
+	List<Address> addresses;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +44,27 @@ public class SubmitPhoto extends Activity {
         gps = new GPSTracker(SubmitPhoto.this);
 		Parse.initialize(this, APP_ID, CLIENT_KEY);
 	}
-	
+	public String getPlaceByLatLng(double lat, double longi){
+		Geocoder geocoder = new Geocoder(SubmitPhoto.this, Locale.getDefault());
+		String addressString = null;
+		try {
+			addresses = geocoder.getFromLocation(lat, longi, 1);
+			StringBuilder place_name = new StringBuilder();
+			if (addresses.size() > 0) {
+				Address address = addresses.get(0);
+				for (int i = 0; i < address.getMaxAddressLineIndex(); i++)
+					place_name.append(address.getAddressLine(i)).append("\n");
+					place_name.append(address.getLocality()).append("\n");
+					place_name.append(address.getPostalCode()).append("\n");
+					place_name.append(address.getCountryName());
+				}
+			addressString = place_name.toString();
+
+		} catch (IOException e) {
+			 Toast.makeText(getApplicationContext(), e+"", Toast.LENGTH_LONG).show();
+		}
+		return addressString;
+	}
 	@Override
 	public void onStart(){
 		super.onStart();
@@ -72,6 +99,9 @@ public class SubmitPhoto extends Activity {
 		   user.put("title", txtTitle.getText().toString());
 		   user.put("description", txtDescription.getText().toString());
 		   user.put("points", location);
+		   
+		   Toast.makeText(getApplicationContext(), getPlaceByLatLng(latitude, longitude).toString()+"", Toast.LENGTH_LONG).show();
+
 		    if (bytearray != null){
 		        ParseFile file = new ParseFile(txtTitle.getText().toString().toLowerCase()+".png",bytearray);
 		        file.saveInBackground();
@@ -89,8 +119,23 @@ public class SubmitPhoto extends Activity {
 		@Override
 		protected void onPostExecute(String str){
 			pd.hide();
+			showDone();
 		}
 	}
+	public void showDone(){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setTitle("Success");
+        alertDialog.setMessage("Disaster Photo successfully submitted.");
+  
+        alertDialog.setNegativeButton("Close", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+            	finish();
+            	dialog.cancel();
+            }
+        });
+  
+        alertDialog.show();
+    }
 	public void clickCancel(View v){
 		finish();
 	}
